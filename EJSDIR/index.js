f01d65b65c2255edd6d5ff97ff08429b7ac4bd20
@vishwajeetsharma09 <2000031822@kluniversity.in>
2024-05-16 00:30:37 +0530

    updated

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -1,6 +1,33 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// app.set("views", path.join(__dirname, "/views")); this is for when we stating the server from outside the directory
+
+// app.get("/", (req, res) => {
+//   res.send("this is home");
+// });
+app.get("/", (req, res) => {
+  res.render("Home.ejs");
+});
+app.get("/instagram/:username", (req, res) => {
+  let { username } = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  // console.log(instaData);
+  if (data) {
+    res.render("instagram", { data });
+  } else {
+    res.render("error.ejs");
+  }
+});
+app.get("/rolldice", (req, res) => {
+  let diceValue = Math.floor(Math.random() * 6 + 1);
+  res.render("rolldice.ejs", { diceValue });
+});
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -6,8 +6,28 @@ app.listen(port, () => {
   console.log(`app is listen on ${port}`);
 });
 
-app.use((req, res) => {
-  console.log(req);
-  console.log("request is received");
-  res.send("this is basic response");
+// app.use((req, res) => {
+//   // console.log(req);
+//   console.log("request is received");
+//   res.send("this is basic response");
+// });
+
+app.get("/", (req, res) => {
+  res.send("you contacted home path");
+});
+app.get("/:username/:id", (req, res) => {
+  let { username, id } = req.params;
+  res.send(`you are landed to @${username}`);
+});
+// app.get("/orange", (req, res) => {
+//   res.send("you contacted orange path");
+// });
+// app.get("*", (req, res) => {
+//   res.send("this path does not exist");
+// });
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  res.send(
+    `<a href="https://vishwajeet09.vercel.app" />you are searching for  ${q}`
+  );
 });
